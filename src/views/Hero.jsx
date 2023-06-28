0fef74641860a784a1c980d54e09e920ade80863
AJ Espinosa <espinosa.aaron45@gmail.com>
2023-06-28 21:52:36 +0800

    major adjustments

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import NET from 'vanta/dist/vanta.net.min'
+import * as THREE from 'three'
 
 const Hero = () => {
+    const [vantaEffect, setVantaEffect] = useState(null)
+    
+
+    const myRef = useRef(null)
+    useEffect(() => {
+        if (!vantaEffect) {
+            setVantaEffect(NET({
+                el: myRef.current,
+                mouseControls: false,
+                touchControls: false,
+                gyroControls: false,
+                minHeight: 200.00,
+                minWidth: 200.00,
+                scale: 1.00,
+                scaleMobile: 1.00,
+                color: 0xffffff,
+                backgroundColor: 0x102240,
+                points: 10.00,
+                maxDistance: 5.00,
+            }))
+        }
+        return () => {
+            if (vantaEffect) vantaEffect.destroy()
+        }
+    }, [vantaEffect])
 
     return (
         <>
-            <div className='min-h-screen flex items-center justify-center w-full h-full bg-[#102240]'>
+            <div className='min-h-screen flex items-center justify-center w-full h-full bg-[#102240] box-border' >
+                <div className="z-0 absolute top-0 left-0 h-screen w-full" ref={myRef}></div>
                 {/* hero content */}
                 <div className='flex flex-col gap-y-20 text-center overflow-hidden font-inter items-center relative w-full h-screen'>
 
@@ -19,8 +47,8 @@ const Hero = () => {
                         </div>
                         <p className='text-white max-w-[30rem] font-extralight'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus suscipit cursus dolor vitae euismod. </p>
                         <div className="flex gap-[1rem] items-center justify-center font-uncial">
-                            <button className="bg-yllw text-white py-[1rem] px-[2rem] rounded-[3rem]">But Token</button>
-                            <button className="bg-transparent text-white py-[1rem] px-[2rem] rounded-[3rem] border-solid border-yllw border-[3px]">Charts</button>
+                            <button className="bg-yllw text-white py-[1rem] px-[2rem] rounded-[7px] border-solid border-yllw border-[2px]">But Token</button>
+                            <button className="bg-transparent text-white py-[1rem] px-[2rem] rounded-[7px] border-solid border-yllw border-[2px]">Charts</button>
                         </div>
                     </div>
 
